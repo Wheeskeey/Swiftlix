9d147e2b3dea99479004fd668aec518f2d79109c
Michał Sadurski <mcplay7@gmail.com>
2021-10-29 23:14:00 +0200

    Initial Commit

---
+++ Swiftlix/SwiftlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftlixApp.swift
+//  Swiftlix
+//
+//  Created by Michał Sadurski on 29/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
